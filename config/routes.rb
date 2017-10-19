41e967d81fea19b5c05e9741c5a7426f47a9af4d
Matt Paoli <mattpaoli@hotmail.com>
2017-10-19 15:43:34 -0400

    updated webpage form

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   resources :webpages
 
-  get 'template' => 'view#model'
+  get 'template' => 'webpages#show'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
